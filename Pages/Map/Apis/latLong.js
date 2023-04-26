6b8dca82e483301af832771b380b108d04b311ec
Samir Patil <55244065+samirpatil2000@users.noreply.github.com>
2023-04-26 23:20:02 +0530

    Feature/connect points with line sam (#3)
    * [NEW] Line added which connects points
    
    * [NEW] Integrated Backend Apis
    
    * [CHANGE] Minor changes
    
    * [FIX] Bug fixes
    
    * [NEW] Login api added

---
+++ Pages/Map/Apis/latLong.js
@@ -0,0 +1,90 @@
+import {AUTH_TOKEN, BACKEND_SERVICE_URL, SERVICE_URL_CYCLIC} from "../../../configurations";
+
+
+export function getData() {
+    return fetch(SERVICE_URL_CYCLIC)
+        .then(res => res.json())
+        .then(data => {
+            let arr = []
+            data.map((x) => {
+                arr.push([x.longitude, x.latitude])
+            })
+            return arr
+        })
+}
+
+export function getAllData() {
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", AUTH_TOKEN);
+
+    var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+
+    return fetch(`${BACKEND_SERVICE_URL}/api/latlongs`, requestOptions)
+        .then(response => response.json())
+        .then(result => { return result })
+        .catch(error => console.log('error', error));
+}
+
+
+
+let pointsArray = [
+  [
+    8111995.823911391,
+    2157436.7405757667
+  ],
+  [
+    8112092.671868383,
+    2157465.196564629
+  ],
+  [
+    8112392.671868383,
+    2157465.196564629
+  ],
+  [
+    8112592.671868383,
+    2157465.196564629
+  ]
+]
+
+let result = {
+    "1": [
+        [
+            8111995.823911391,
+            2157436.7405757667
+        ],
+        [
+            8112092.671868383,
+            2157465.196564629
+        ],
+        [
+            8112392.671868383,
+            2157465.196564629
+        ],
+        [
+            8112592.671868383,
+            2157465.196564629
+        ]
+    ],
+    "2": [
+        [
+            8111895.823911391,
+            2157436.7405757667
+        ],
+        [
+            8112192.671868383,
+            2157465.196564629
+        ],
+        [
+            8112292.671868383,
+            2157465.196564629
+        ],
+        [
+            8112692.671868383,
+            2157565.196564629
+        ]
+    ]
+}
--- Pages/Map/main.js
+++ Pages/Map/main.js
@@ -9,7 +9,12 @@ import { easeOut } from 'ol/easing.js';
 import { fromLonLat } from 'ol/proj.js';
 import { getVectorContext } from 'ol/render.js';
 import { unByKey } from 'ol/Observable.js';
-import { MultiPoint } from 'ol/geom';
+import {Circle, LineString, MultiPoint} from 'ol/geom';
+import {Fill} from "ol/style";
+import {lineStyle, pointStyle} from "./Styles/styles.js"
+import {getAllData, getData} from "./Apis/latLong.js"
+import {BoatConfig} from "./constants";
+
 
 const tileLayer = new TileLayer({
   source: new OSM({
@@ -24,43 +29,96 @@ const vector = new VectorLayer({
   source: source,
 });
 
+
 const map = new Map({
   layers: [tileLayer, vector],
   target: 'map',
   view: new View({
-    center: [8120000, 2145000],
-    zoom: 12,
+    center: [8110000, 2155000],
+    zoom: 14,
     multiWorld: true,
   }),
 });
 
 
-const getBtn = document.getElementById("get-data")
-const postData = document.getElementById("post-data")
+async function addDataInMap(boatId, coordinates) {
+  const lineColor = BoatConfig[boatId]["lineColor"]
+  const pointColor = BoatConfig[boatId]["pointColor"]
+  coordinates.sort()
 
-function getData() {
-  return fetch("https://hungry-cowboy-boots-boa.cyclic.app")
-    .then(res => res.json())
-    .then(data => {
-      let arr = []
-      data.map((x) => {
-        arr.push(fromLonLat([x.longitude, x.latitude]))
-        // console.log(x.latitude,x.longitude,x.boatId)
-        // postData.innerText = `Latitude : ${x.latitude} Longitude: ${x.longitude} ID: ${x.boatId}`
-      })
-      return arr
-    })
+  let pointFeatures = coordinates.map(point => {
+    let geom = new Point(fromLonLat(point));
+    let feature = new Feature(geom);
+    feature.setStyle(pointStyle(pointColor))
+    return feature;
+  });
+  source.addFeatures(pointFeatures);
+  console.log("Sources", source)
+
+  if (coordinates.length > 1) {
+    let lineCoords = coordinates.map(point => {
+      return fromLonLat(point);
+    });
+    let geom = new LineString(lineCoords);
+    let feature = new Feature(geom);
+    feature.setStyle(lineStyle(lineColor));
+    source.addFeature(feature);
+  }
 }
 
-async function addRandomFeature() {
-  var pointsArray = []
-  pointsArray = await getData().then(x => { return x })
-  // console.log(pointsArray)
-  let geom = new MultiPoint(pointsArray);
-  let feature = new Feature(geom);
-  source.addFeature(feature);
-  pointsArray = []
+
+let result = {
+  "1": [
+    [ 72.87129833333333, 19.02124 ],
+    [ 72.8722072112705, 19.022486883444827 ],
+    [ 72.8717249624107, 19.021112912277605 ],
+    [ 72.87194808539974, 19.021814590997296 ],
+    [ 72.87264912891828, 19.022731462758387 ],
+    [ 72.87296472056748, 19.022359792961505 ],
+    [ 72.87369534803419, 19.022652422979674 ],
+    [ 72.87407977988146, 19.021825192659693 ],
+    [ 72.87419921678132, 19.02252111572918 ],
+    [ 72.8730512732237, 19.021863325485146 ]
+  ],
+  "2": [
+    [ 72.87049005307729, 19.022096178311442 ],
+    [ 72.87138890071002, 19.022250130372607 ],
+    [ 72.87071627457045, 19.023623058296072 ],
+    [ 72.87176135109955, 19.02399735965082 ],
+    [ 72.87215950827513, 19.02307062328522 ],
+    [ 72.87243438036985, 19.022355290140953 ],
+    [ 72.87126908485081, 19.02258620637172 ],
+    [ 72.87033372410158, 19.023679690461365 ],
+    [ 72.87012949045794, 19.023246388811624 ],
+    [ 72.87086650316967, 19.023291300916934 ]
+  ],
+  "3": [
+    [ 72.87196449967084, 19.018225933032788 ],
+    [ 72.87213539934585, 19.020124088804216 ],
+    [ 72.87141070671363, 19.01757610635317 ],
+    [ 72.87066032735646, 19.01708088008981 ],
+    [ 72.87139334062138, 19.0149173118458 ],
+    [ 72.87174734076103, 19.016300062097212 ],
+    [ 72.87239633384189, 19.01883519740361 ],
+    [ 72.8731338057982, 19.016416826774076 ],
+    [ 72.8729896473579, 19.013898749509465 ]
+  ]
 }
+
+
+async function loadMap() {
+  let response = await getAllData()
+  let data = response.data
+
+  for (let key in data) {
+    let coordinates = data[key].map(obj => [obj.longitude, obj.latitude])
+    console.log(coordinates, "key", key)
+    addDataInMap(key, coordinates)
+  }
+}
+
+// loadMap()
+
 
 const duration = 5000;
 function flash(feature) {
@@ -98,8 +156,9 @@ function flash(feature) {
   }
 }
 
-source.on('addfeature', function (e) {
-  flash(e.feature);
-});
+// source.on('addfeature', function (e) {
+//   flash(e.feature);
+// });
 
-window.setInterval(addRandomFeature, 2000)
+window.setInterval(loadMap, 3000)
+
